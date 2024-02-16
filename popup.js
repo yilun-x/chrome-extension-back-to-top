425c524d06479995313ccc80a3e2c61e8c15a3fb
yilun <243228066@qq.com>
2024-02-16 14:13:43 +0800

    Add files via upload

--- popup.js
+++ popup.js
@@ -5,3 +5,11 @@ newButton.appendChild(textnode);
 newButton.id = "backtotop";
 newButton.href = "#top";
 document.body.append(newButton);
+document.getElementById('backtotop').style.display = "none";
+document.addEventListener("scrollend", (event) => {
+    if (window.scrollY != 0) {
+        document.getElementById('backtotop').style.display = "block";
+    } else {
+        document.getElementById('backtotop').style.display = "none";
+    }
+  });
